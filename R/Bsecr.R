661c9ba220b615807d59ae15148d825359463e1d
Mike Meredith <mmeredith@wcsmsia>
2017-10-27 21:33:24 +0800

    Bsecr revamped

--- R/Bsecr.R
+++ R/Bsecr.R
@@ -2,11 +2,14 @@
 # Bayesian version of secr to work with stoats data
 
 Bsecr0 <- function(capthist, buffer = 100, start=NULL, nAug = NA,
+                    maxSig = 2*buffer,
                     chains=3, sample=1e4, burnin=0, thin=1, adapt=1000,
                     priorOnly=FALSE, parallel=NULL, seed=NULL) {
 
   if(!inherits(capthist, "capthist"))
     stop("'capthist' is not a valid capthist object.")
+  if(length(dim(capthist)) < 3)
+    stop("'capthist' object not compatible with secr v.3, please upgrade.")
 
   startTime <- Sys.time()
 
@@ -23,31 +26,21 @@ Bsecr0 <- function(capthist, buffer = 100, start=NULL, nAug = NA,
 
   # Get starting values, etc from secr.fit
   if(!is.null(start) && inherits(start, "secr")) {
-    mle.res <- predict(start)
+    mle.res <- predict(start)[, 2]
   } else {
-    message("Running secr.fit to get starting values...", appendLF=FALSE) ; flush.console()
-    mle.res <- predict(secr::secr.fit(capthist, buffer=buffer, trace=FALSE))
-    message("done") ; flush.console()
+    mask <- secr::make.mask(traps, buffer)
+    mle.res <- unlist(secr::autoini(capthist, mask))
   }
 
   if(is.na(nAug))
-    nAug <- ceiling(1.5 * mle.res[1, 5] * A)
-  maxSig2 <- (2.2 * mle.res[3, 5]) ^ 2
-  lam0start <- mle.res[2,2]
-  sigma2start <- 2 * mle.res[3,2]^2
-  psistart  <- (mle.res[1,2] * A) / nAug
+    nAug <- ceiling(3 * mle.res[1] * A)
+  psistart  <- (mle.res[1] * A) / nAug
 
   # Convert capture histories into an Animals x Traps matrix
   nInd <- dim(capthist)[1]
   nOcc <- dim(capthist)[2]
   yMat <- matrix(0, nAug, J)
-  if(length(dim(capthist)) == 3) {
-    yMat[1:nInd,] <- apply(capthist, c(1,3), sum)
-  } else {
-    for(i in 1:nInd)
-      for(j in 1:nOcc)
-        yMat[i, capthist[i, j]] <- yMat[i, capthist[i, j]] + 1
-  }
+  yMat[1:nInd,] <- apply(capthist, c(1,3), sum)
 
   # Get initial locations of animals
   SX <- SY <- numeric(nInd)
@@ -61,19 +54,18 @@ Bsecr0 <- function(capthist, buffer = 100, start=NULL, nAug = NA,
   modelFile <- file.path(tempdir(), "JAGSmodel.txt")
   modeltext <- "
     model {
-    sigma2 ~ dunif(0, maxSig2)      # need to set good max
-    sigma <- sqrt(sigma2 / 2)
-    lam0 ~ dgamma(0.1, 0.1)
+    sigma ~ dunif(0, maxSig)      # need to set good max
+    sigma2 <- 2*sigma^2
+    g0 ~ dunif(0, 1)
     psi ~ dunif(0, 1)
     for (i in 1:M){         #loop through the augmented population
       z[i] ~ dbern(psi)     #state of individual i (real or imaginary)
       SX[i] ~ dunif(xl, xu) #priors for the activity centre for each individual
       SY[i] ~ dunif(yl, yu) #  xl, yl = lower coordinate; xu, yu = upper value
-      for(j in 1:J) {       #loop through the J camera trap locations
+      for(j in 1:J) {       #loop through the J detector locations
          Dsq[i,j] <- pow(SX[i]-trapmat[j,1], 2) + pow(SY[i]-trapmat[j,2],2)
-         g[i,j] <- exp(-Dsq[i,j]/sigma2)
-         lambda[i,j] <- K * g[i,j] * lam0 * z[i]
-         y[i,j] ~ dpois(lambda[i,j])
+         g[i,j] <- z[i] * g0 * exp(-Dsq[i,j]/sigma2)
+         y[i,j] ~ dbin(g[i,j], K)
       }
     }
     N <- sum(z[1:M]) # derive number (check against M)
@@ -84,15 +76,15 @@ Bsecr0 <- function(capthist, buffer = 100, start=NULL, nAug = NA,
    # organise the data:
   jagsData <- list(M = nAug, xl=xl, xu=xu, yl=yl, yu=yu, J=J,
       trapmat=as.matrix(traps), K=nOcc,
-      A = A, maxSig2 = maxSig2)
+      A = A, maxSig = maxSig)
   if (!priorOnly)
     jagsData$y <- yMat
   inits <- function() {list(z=rep(1, nAug),
                         SX=c(SX, runif(nAug-nInd, xl, xu)),
                         SY=c(SY, runif(nAug-nInd, yl, yu)),
-                        sigma2=sigma2start, lam0=lam0start,
+                        sigma=mle.res[3], g0=mle.res[2],
                         psi=psistart)}
-  wanted <- c("D", "lam0", "sigma")
+  wanted <- c("D", "g0", "sigma")
 
   # Run the model:
   resB <- justRunJags(jagsData, inits, wanted, modelFile,
