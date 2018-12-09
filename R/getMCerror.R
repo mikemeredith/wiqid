ffc8b24857f41800a9d7a0308f69cd46e98fc034
mikemeredith <mmeredith@wcs.org>
2018-12-09 15:35:33 +0800

    Added getMCerror

---
+++ R/getMCerror.R
@@ -0,0 +1,53 @@
+
+# Calculation of MCMC error
+
+# See Lunn et al 2013, The BUGS Book, p77.
+
+# Lunn et al want number of batches = batch size = sqrt(chain length), but only
+#   apply that to a single chain; they comment that with multiple chains, number of
+#   batches will be larger.
+# If there are many chains (eg, 20), this means number of batches >> batch size.
+# To achieve number of batches = batch size, we use sqrt(total iterations),
+#   corrected to be a multiple of number of chains.
+
+# It gives the same result as coda::batchSE with an appropriate choice of batchSize.
+
+getMCerror <- function(object, n.chains, SDpc=FALSE) {
+  x <- as.matrix(object)                    # make sure it's a matrix
+  # oldx <- x
+  if(missing(n.chains) || !is.numeric(n.chains)) {
+    n.chains <- attr(object, "n.chains")
+    if(is.null(n.chains))
+     stop("Please supply a value for 'n.chains'!", call.=FALSE)
+  }
+  if(nrow(x) %% n.chains > 0)
+    stop("The number of rows in 'x' must be a multiple of 'n.chains'.", call.=FALSE)
+  n.par <- ncol(x)                     # number of parameters
+  parNames <- colnames(x)
+  n.iter <- nrow(x)                    # total sample size
+  ipc <- n.iter / n.chains             # iterations per chain (T in Lunn et al)
+
+  bpc <- sqrt(n.iter) %/% n.chains     # batches per chain (Q)
+  bsize <- floor(nrow(x)/n.chains/bpc) # batch size (a)
+  ni <- bpc * bsize                    # number of iters per chain to use
+
+  dim(x) <- c(ipc, n.chains, n.par)        # separate the chains
+  x <- x[1:ni, , ]                         # discard unwanted iters
+  dim(x) <- c(bsize, bpc, n.chains, n.par) # separate the batches
+  bm <- apply(x, 2:4, mean)                # get batch means
+  dim(bm) <- c(bpc*n.chains, n.par)        # Combine bm's across chains
+  sqdev <- (sweep(bm, 2, colMeans(bm), "-"))^2  # Get squared deviation
+  SD <- sqrt(colSums(sqdev) * bsize / (n.chains*bpc - 1)) # sqrt(rho)
+  MCE <- SD / sqrt(n.iter)
+  names(MCE) <- parNames
+  if(SDpc) {
+    return(100 * MCE / apply(object, 2, sd))
+  } else {
+    return(MCE)
+  }
+}
+
+
+
+
+
--- R/simpleRhat.R
+++ R/simpleRhat.R
@@ -5,41 +5,43 @@
 
 # This follows WinBUGS in using the central 80% interval as the measure of width (WinBUGS manual p.27).
 
+simpleRhat <- function(object, n.chains, burnin=0) {
 
-# Calculate Rhat for a vector; called by simpleRhat; not exported
-
-simpleRhat1 <- function(x, n.chains, burnin=0) {
-  if(!is.numeric(x))
-    return(NA_real_)
   width <- function(y)
     diff(quantile(y, c(0.1, 0.9)))
-  xmat <- matrix(x, ncol=n.chains)
-  if(burnin) {
-    discard <- round(nrow(xmat)*burnin)
-    xmat <- xmat[-(1:discard), ]
-  }
-  unname(width(xmat) / mean(apply(xmat, 2, width)))
-}
-#.......................................................................
 
-#
-simpleRhat <- function(x, n.chains, burnin=0) {
-  if(missing(n.chains) || !is.numeric(n.chains))
-    stop("Please supply a value for 'n.chains'!", call.=FALSE)
+  x <- as.matrix(object)
+  if(missing(n.chains) || !is.numeric(n.chains)) {
+    n.chains <- attr(object, "n.chains")
+    if(is.null(n.chains))
+     stop("Please supply a value for 'n.chains'!", call.=FALSE)
+  }
   if(n.chains < 2)
     stop("More than 1 chain is needed to calculate Rhat.", call.=FALSE)
+  if(nrow(x) %% n.chains > 0)
+    stop("The number of rows in 'x' must be a multiple of 'n.chains'.", call.=FALSE)
   if(burnin < 0 || burnin > 0.9)
     stop("'burnin' must be between 0 and 0.9.", call.=FALSE)
-  if(is.matrix(x) || is.data.frame(x)) {
-    if(nrow(x) %% n.chains > 0)
-      stop("The number of rows in 'x' must be a multiple of 'n.chains'.", call.=FALSE)
-    return(apply(x, 2, simpleRhat1, n.chains=n.chains, burnin=burnin))
-  } else if(is.vector(x)) {
-    if(length(x) %% n.chains > 0)
-      stop("The length of 'x' must be a multiple of 'n.chains'.", call.=FALSE)
-    return(simpleRhat1(x, n.chains=n.chains, burnin=burnin))
-  } else {
-    stop("'x' must be a numeric vector, matrix or data frame.", call.=FALSE)
-  }
-}
 
+  n.par <- ncol(x)                     # number of parameters
+  parNames <- colnames(x)
+  n.iter <- nrow(x)                    # total sample size
+  ipc <- n.iter / n.chains             # iterations per chain (T)
+
+  dim(x) <- c(ipc, n.chains, n.par)    # separate the chains
+  if(burnin) {
+    discard <- round(ipc*burnin)       # iters to discard as burn-in
+    x <- x[-(1:discard), , ]           # discard unwanted iters
+    ipc <- ipc - discard
+  }
+
+  W0 <- apply(x, 2:3, width)           # width of individual chains
+  W <- colMeans(W0)
+  
+  dim(x) <- c(ipc * n.chains, n.par)   # combine the chains (after burn-in)
+  B <- apply(x, 2, width)              # width of pooled chains
+
+  Rhat <- B / W
+  names(Rhat) <- parNames
+  return(Rhat)
+}
